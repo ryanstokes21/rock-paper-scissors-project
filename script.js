dfbedce048f67e822568ba9102872bfbc01abfcf
Ryan Stokes <ryanstokescfbre@gmail.com>
2025-12-26 13:47:03 -0600

    get user choice with prompt and IF/ELSE statements

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ function getComputeChoice() {
   if (computerChoice <= 0.33) {
     console.log( computerChoice, 'Rock');
   } else if (computerChoice <= 0.66) {
-    console.log(computerChoice, 'paper');
+    console.log(computerChoice, 'Paper');
   } else {
     console.log(computerChoice, 'Scissors');
   }
 }
 
+function getUserChoice() {
+  let answer = prompt('Rock, Paper, or Scissors?').toLowerCase();
+  switch (answer) {
+    case 'rock':
+      console.log('Rock');
+      break;
+    case 'paper':
+      console.log('Paper');
+      break;
+    case 'scissors':
+      console.log('Scissors');
+      break;
+    default:
+      console.log('Pick Rock, Paper, or Scissors');
+  }
+}
+
+getUserChoice();
 getComputeChoice();
