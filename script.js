95025eed7a90d3758ed74af5d734404b989dc14f
Ryan Stokes <ryanstokescfbre@gmail.com>
2025-12-26 13:17:57 -0600

    get the computer choice for rock paper scissors.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('working...')
+// let userScore = 0;
+// let computerScore = 0;
+
+function getComputeChoice() {
+  let computerChoice = Math.random();
+  if (computerChoice <= 0.33) {
+    console.log( computerChoice, 'Rock');
+  } else if (computerChoice <= 0.66) {
+    console.log(computerChoice, 'paper');
+  } else {
+    console.log(computerChoice, 'Scissors');
+  }
+}
+
+getComputeChoice();
