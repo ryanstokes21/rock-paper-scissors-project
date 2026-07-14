46c3272a1ff1e3e788d922c04e9256c422940145
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-07-14 15:52:11 -0500

    add event listeners to buttons
    added event listeners to buttons to get the user
    choice. this removes the previous function to get
    the users choice. added a couple helper functions
    to display round results and score.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const resultsEl = document.getElementById('result');
+const scoreEl = document.getElementById('score');
+
 //get a random number
 function getRandomNumber() {
   const rndNum = Math.floor(Math.random() * 3) + 1;
@@ -22,69 +25,51 @@ function getComputerChoice() {
   }
 }
 
-function getUserChoice() {
-  //prompt for users choice for Rock Paper or Scissors
-  let userChoice = prompt('Choose Rock, Paper, or Scissors');
-  const normalizeChoice = userChoice.trim().toLowerCase();
-
-  //user inputs choice
-  //If user inputs Rock Paper of Scissors
-  //display choice in console.
-  switch (normalizeChoice) {
-    case 'rock':
-      return 'rock';
-      break;
-    case 'paper':
-      return 'paper';
-      break;
-    case 'scissors':
-      return 'scissors';
-      break;
-  }
+function displayUserWins(userChoice, computerChoice) {
+  resultsEl.textContent = `You win!! ${userChoice} beats ${computerChoice}`;
 }
 
-function playGame() {
-  let userScore = 0;
-  let computerScore = 0;
+function displayScore() {
+  scoreEl.textContent = `User score: ${userScore} - computer score: ${computerScore}`;
+}
+let userScore = 0;
+let computerScore = 0;
 
-  function playRound() {
-    const userChoice = getUserChoice();
-    const computerChoice = getComputerChoice();
-    //compare user and computer choices
-    if (userChoice === computerChoice) {
-      console.log('Tie!');
-    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
-      console.log(`You win!! ${userChoice} beats ${computerChoice}`);
-      userScore++;
-      console.log(
-        `User score: ${userScore} - computer score: ${computerScore}`,
-      );
-    } else if (userChoice === 'paper' && computerChoice === 'rock') {
-      console.log(`You win!! ${userChoice} beats ${computerChoice}`);
-      userScore++;
-      console.log(
-        `User score: ${userScore} - computer score: ${computerScore}`,
-      );
-    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
-      console.log(`You win!! ${userChoice} beats ${computerChoice}`);
-      userScore++;
-      console.log(
-        `User score: ${userScore} - computer score: ${computerScore}`,
-      );
-    } else {
-      console.log(`You lose!! ${computerChoice} beats ${userChoice}`);
-      computerScore++;
-      console.log(
-        `User score: ${userScore} - computer score: ${computerScore}`,
-      );
-    }
-    //if there is a winner
-    //increment the score for the winner by 1
-    //Otherwise its a tie and the score does not increment
+function playRound(userSelection) {
+  const userChoice = userSelection;
+  const computerChoice = getComputerChoice();
+  //compare user and computer choices
+  if (userChoice === computerChoice) {
+    resultsEl.textContent = 'Tie';
+  } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+    displayUserWins(userChoice, computerChoice);
+    userScore++;
+    displayScore(userChoice, computerChoice);
+  } else if (userChoice === 'paper' && computerChoice === 'rock') {
+    displayUserWins(userChoice, computerChoice);
+    userScore++;
+    displayScore();
+  } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+    displayUserWins();
+    userScore++;
+    displayScore();
+  } else {
+    resultsEl.textContent = `You lose!! ${computerChoice} beats ${userChoice}`;
+    computerScore++;
+    displayScore();
   }
-  for (let r = 1; r <= 5; r++) {
-    playRound();
-  }
+  //if there is a winner
+  //increment the score for the winner by 1
+  //Otherwise its a tie and the score does not increment
 }
 
-playGame();
+const btnContainer = document.getElementById('btn-container');
+
+btnContainer.addEventListener('click', (e) => {
+  const button = e.target.closest('.btn');
+
+  if (!button || !btnContainer.contains(button)) return;
+
+  const value = button.dataset.value;
+  playRound(value);
+});
